75ff97b9968e19fa3f951ef67774d5abf11f4232
Enzo AIT <enzo.ait_yakoub@edu.devinci.fr>
2022-12-20 19:09:25 +0100

    modification variable connexion

--- Serveur/Modèles/Connexion.js
+++ Serveur/Modèles/Connexion.js
@@ -3,8 +3,8 @@ const Sequelize = require("sequelize");
 const URL_BDD =
   process.env.URL_BDD || "mysql://Groupe12:password123@localhost:3306/bdd_taskmanager_groupe12";
 
-const connection = new Sequelize(URL_BDD);
+const connexion = new Sequelize(URL_BDD);
 
-connection.authenticate().then(() => console.log("La connexion à la BDD a été établie"));
+connexion.authenticate().then(() => console.log("La connexion à la BDD a été établie"));
 
-module.exports = connection;
+module.exports = connexion;
