54f0daed5cefbe10d86806c5a7773c2a5d10818a
Enzo AIT <enzo.ait_yakoub@edu.devinci.fr>
2022-12-28 15:48:18 +0100

    remplacer le commentaire par le lien de sa bdd puis remettre ce commentaire une fois qu'on a fini

--- Serveur/Modèles/Connexion.js
+++ Serveur/Modèles/Connexion.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const URL_BDD =
-  process.env.URL_BDD || "mysql://Groupe12:password123@localhost:3306/bdd_taskmanager_groupe12";
+const URL_BDD = 
+  process.env.URL_BDD || //Mettre le lien de sa bdd ici;
 
 const connexion = new Sequelize(URL_BDD);
 
