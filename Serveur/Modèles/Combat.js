8ae224d2acd5bff2400904a88fa5a6b7fc2f6d55
Jilani235 <94959107+Jilani235@users.noreply.github.com>
2023-01-04 19:46:07 +0000

    Ajout attribut modèle combat

--- Serveur/Modèles/Combat.js
+++ Serveur/Modèles/Combat.js
@@ -2,10 +2,16 @@ const { Model, DataTypes } = require("sequelize");
 const connexion = require("./Connexion");
 class Combat extends Model {}
 
-Combat.init({
-
+Combat.init(
+    {
+        prénom1: DataTypes.STRING,
+        prénom2: DataTypes.STRING,
+        vainqueur: DataTypes.STRING,
+        puissance_restante: DataTypes.INTEGER,
+        points_de_vie_restant: DataTypes.INTEGER,
+    },
 {
- sequelize: connexion,
+sequelize: connexion,
 }
-});
+);
 module.exports = Combat;
