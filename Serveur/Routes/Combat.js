d2628e9a4a7050c24a1b9927fbdc6642102a0d40
Jilani235 <94959107+Jilani235@users.noreply.github.com>
2023-01-10 14:09:57 +0000

    Mise à jour combat.js (Demande autorisation)

--- Serveur/Routes/Combat.js
+++ Serveur/Routes/Combat.js
@@ -1,55 +1,81 @@
 const { Router } = require("express");
 const Interdiction = require("../Erreurs/Interdiction");
+const CheckAutorisation = require("../Middlewares/CheckAutorisation");
+const CheckRole = require("../Middlewares/CheckRole");
 const { Combat } = require("../Modèles");
 
 const router = new Router();
 
 // Get collection
 router.get(
-    "/combat", 
-    (req, res) => {
-      Combat.findAll({
-        where: req.query,
-      }).then((data) => res.json(data));
+  "/combat",
+  CheckAutorisation, 
+  CheckRole({ minRole: CheckRole.ROLES.ADMINISTRATEUR}), 
+  (req, res) => {
+    Combat.findAll({
+      where: req.query,
+    }).then((data) => res.json(data));
+  }
+);
+
+// Créer un combat
+router.post("/combat", CheckAutorisation,(req, res, next) => {
+  if (req.utilisateur.id !== req.body.UtilisateurId) throw new Interdiction();
+  const Combat = new Combat(req.body);
+  combat
+    .save()
+    .then((data) => res.status(201).json(data))
+    .catch(next);
+});
+
+// Récupérer un combat
+router.get("/combat/:id", CheckAutorisation, async(req, res) => {
+
+  const utilisateur_id = await Combat.findOne({
+    attributes: ['UtilisateurId'],
+    where: {
+      id: parseInt(req.params.id)
+    },
+    raw: true
+  });
+
+  if(!utilisateur_id){
+    res.sendStatus(404);
+  } else {
+
+    if (req.utilisateur.id !== utilisateur_id.UtilisateurId){
+      res.sendStatus(403);
+    } 
+    else{
+      const combat = await Combat.findByPk(parseInt(req.params.id));
+      if (!combat) {
+        res.sendStatus(404);
+      } else {
+        res.json(combat);
+      }
     }
-  );
+
+  }
+
+});
+
+// Update un combat
+router.put("/combat/:id", CheckAutorisation, (req, res, next) => {
+  if (req.utilisateur.id !== req.body.UtilisateurId) throw new Interdiction();
+  Combat.update(req.body, {
+  where: { id: parseInt(req.params.id) },
+  individualHooks: true,
+})
+  .then(([nbUpdated]) => {
+    if (!nbUpdated) return res.sendStatus(404);
+    Combat.findByPk(parseInt(req.params.id)).then((Combat) => res.json(Combat));
+  })
+  .catch(next);
+});
+
+// Delete un combat
+router.delete("/combat/:id", CheckAutorisation, CheckRole({ minRole: CheckRole.ROLES.ADMINISTRATEUR}), (req, res) => {
   
-  // Créer un combat
-  router.post("/combat", (req, res, next) => {
-    const combat = new Combat(req.body);
-    combat
-      .save()
-      .then((data) => res.status(201).json(data))
-      .catch(next);
-  });
-  
-  // Récupérer un combat
-  router.get("/combat/:id", async (req, res) => {
-    const combat = await Combat.findByPk(parseInt(req.params.id));
-    if (!combat) {
-      res.sendStatus(404);
-    } else {
-      res.json(combat);
-    }
-  });
-  
-  // Update un combat
-  router.put("/combat/:id", (req, res, next) => {
-    if (req.combat.id !== parseInt(req.params.id)) throw new Interdiction();
-    Combat.update(req.body, {
-      where: { id: parseInt(req.params.id) },
-      individualHooks: true,
-    })
-      .then(([nbUpdated]) => {
-        if (!nbUpdated) return res.sendStatus(404);
-        Combat.findByPk(parseInt(req.params.id)).then((combat) => res.json(combat));
-      })
-      .catch(next);
-  });
-  
-  // Delete un combat
-  router.delete("/combat/:id", (req, res) => {
-    if (req.combat.id !== parseInt(req.params.id)) throw new Interdiction();
     Combat.destroy({
       where: {
         id: parseInt(req.params.id),
@@ -61,6 +87,7 @@ router.get(
         res.sendStatus(404); 
       }
     });
-  });
-  
-  module.exports = router;
+
+});
+
+module.exports = router;
